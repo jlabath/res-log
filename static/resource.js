b4d1633314a2ef349a29f6b81fa880d2cf94fc58
Jakub Labath <jakubl@gadventures.com>
2015-04-17 04:02:10 +0000

    implement history

--- static/resource.js
+++ static/resource.js
@@ -31,8 +31,46 @@ $(function(){
             return 0;
          }
     });
-    //ok def resources in here
+
+    //defs for history
+    var HistoryItem = Backbone.Model.extend({
+        defaults: function() {
+            return {
+                label: "Departures: -1",
+                rid: "-1",
+                rtype: "departures"
+            };
+        }
+    });
+
+    var HistoryCollection = Backbone.Collection.extend({
+         model: HistoryItem
+    });
+
+    var rhistory = new HistoryCollection();
+    var HistoryView = Backbone.View.extend({
+        template: _.template($('#res-history').html()),
+        el: $("#history"),
+        render: function() {
+            this.$el.html(this.template(this));
+        },
+        initialize: function() {
+            this.listenTo(rhistory, 'add', this.render);
+        },
+        events: {
+            "click li": "loadHi"
+        },
+        loadHi: function(evt){
+            var clickedLi = $(evt.target);
+            var hi = this.collection.at(clickedLi.index());
+            $("#restype").val(hi.get("rtype"));
+            $("#resid").val(hi.get("rid"));
+            $("#resgo").trigger($.Event("click"));
+        }
+    });
+
     var resources = new ResList();
+    var hview = new HistoryView({collection: rhistory});
     //our view that listens to changes in resources
     var AppView = Backbone.View.extend({
         el: $("#resapp"),
@@ -61,6 +99,17 @@ $(function(){
             });
             if (resources.length >  0){
                 this.$("#reslst").change();
+                //we had results add this restype and resid and label to history
+                var restype = this.$("#restype");
+                var resid = this.$("#resid");
+                var h = new HistoryItem({
+                    label: restype.find("option:selected").text()+": "+resid.val(),
+                    rid: resid.val(),
+                    rtype: restype.val()
+                });
+                if (rhistory.where({rid: h.get("rid"), rtype: h.get("rtype")}).length === 0){
+                    rhistory.add(h, {at: 0});
+                }
             } else {
                 lst.append($("<option>").html("No results found for: " + resources.url.substring(3) + " ").attr('value',''));
                 this.$("#resview").hide();
